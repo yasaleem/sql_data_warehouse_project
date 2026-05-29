4b29949c779c14eaeb5239e342043cb947090f00
yasaleem <yassir.saleem@hotmail.com>
2026-05-29 13:48:19 +0400

    Add stored procedure for loading staging layer
    This stored procedure performs the ETL process to populate the 'staging' schema tables from the 'raw' schema, including truncating staging tables and inserting transformed data.

--- scripts/staging/procedure_load_staging.sql
+++ scripts/staging/procedure_load_staging.sql
@@ -1,1 +1,19 @@
+============================================================================================================
+Stored procedure: Load Staging Layer (Raw -> Staging)
+============================================================================================================
+Purpose:
+  This Procedure performs the ETL (Extract, Transform, Load) process to populate the 'staging' schema tables
+  from the 'raw' schema.
+Actions Performed:
+  - Truncate Staging tables
+  - Inserts transformed and cleansed data from Raw into Staging tables.
+
+Parameters:
+  None.
+  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+  EXEC Staging.load_staging;
+============================================================================================================
+
 
