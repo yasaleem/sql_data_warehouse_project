84a2c27007f440c14be084ba7a9bd987a7ad0512
yasaleem <yassir.saleem@hotmail.com>
2026-05-29 13:29:58 +0400

    Create staging tables for CRM and orders
    This script creates staging tables for CRM customers, orders, and order items, dropping existing tables if they exist.

--- scripts/staging/ddl.staging.sql
+++ scripts/staging/ddl.staging.sql
@@ -1,1 +1,37 @@
+=================================================================================================
+DDL Script: Create the staging Tables
+  This script create tables in the 'staging' schema, dropping existing tables if they already exist.
+  Run this script to re-define the DDL of the 'staging' Tables.
+=================================================================================================
 
+-- staging CRM customers table
+IF OBJECT_ID('staging.crm_system', 'U') is NOT NULL
+    DROP TABLE staging.crm_system
+CREATE TABLE staging.crm_customers (
+    Customer_ID     FLOAT,
+    Email           NVARCHAR(255),
+    City            NVARCHAR(100),
+    Country         NVARCHAR(100),
+    Registration_Date NVARCHAR(50)
+);
+
+-- staging orders table
+IF OBJECT_ID('staging.orders', 'U') is NOT NULL
+    DROP TABLE staging.orders
+CREATE TABLE staging.erp_orders (
+    Invoice         NVARCHAR(20),
+    Customer_ID     FLOAT,
+    InvoiceDate     NVARCHAR(50) 
+);
+GO
+
+-- staging order items 
+IF OBJECT_ID('staging.order_items', 'U') is NOT NULL
+    DROP TABLE staging.order_items
+CREATE TABLE staging.erp_order_items (
+    Invoice         NVARCHAR(20),
+    StockCode       NVARCHAR(20),
+    Description     NVARCHAR(255),
+    Quantity        FLOAT,
+    Price           FLOAT
+);
